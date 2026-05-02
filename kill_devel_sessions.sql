91dc5557b66fb820a92c81bb1d189c339afd8815
Dmitry Kremer <djester@users.noreply.github.com>
2026-05-02 12:47:17 +0300

    Create kill_devel_sessions.sql

---
+++ kill_devel_sessions.sql
@@ -0,0 +1,18 @@
+create function kill_devel_sessions() returns void as $$
+select pg_terminate_backend(pid) 
+  from pg_stat_activity pga
+ where backend_type = 'client backend' 
+   and application_name is not null 
+   and client_addr is not null 
+   and datname <> 'postgres'
+   and not (select datistemplate from pg_database pdb where pdb.datname=pga.datname) 
+   and (
+   		   application_name ilike '%dbeaver%'
+   		or application_name ilike '%datagri%'
+   		or application_name ilike '%pgadmin%'
+   	   )
+   and state <>'idle'
+   and pid<>pg_backend_pid()
+   and coalesce(clock_timestamp()-xact_start, clock_timestamp()-query_start) > interval '2 minutes'
+;
+$$ language sql;
