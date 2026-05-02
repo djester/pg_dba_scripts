417dff362eeb305523ff06446e47be1940085582
Dmitry Kremer <djester@users.noreply.github.com>
2026-05-02 15:31:53 +0300

    add default value arguments and change return type

--- kill_devel_sessions_func.sql
+++ kill_devel_sessions_func.sql
@@ -1,4 +1,4 @@
-create or replace function kill_devel_sessions() returns void as $$
+create function kill_devel_sessions(p_time interval default interval '2 minutes') returns boolean as $$
 select pg_terminate_backend(pid) 
   from pg_stat_activity pga
  where backend_type = 'client backend' 
@@ -13,6 +13,7 @@ select pg_terminate_backend(pid)
    	   )
    and state <>'idle'
    and pid<>pg_backend_pid()
-   and coalesce(clock_timestamp()-xact_start, clock_timestamp()-query_start) > interval '2 minutes'
+   and coalesce(clock_timestamp()-xact_start, clock_timestamp()-query_start) > p_time
 ;
 $$ language sql;
+
